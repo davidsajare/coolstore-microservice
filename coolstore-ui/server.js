7c06ae40583e911806e175139c8a907e21159448
gaguo <gaguo@vmware.com>
2020-09-22 16:34:03 +0800

    fix server.js

--- coolstore-ui/server.js
+++ coolstore-ui/server.js
@@ -17,12 +17,14 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 // Enable CORS support
 app.use(cors());
-app.options('*', cors());
 
 
 
 // error handling
 app.use(function (err, req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
     console.error(err.stack);
     res.status(500).send('Something bad happened!');
 });
